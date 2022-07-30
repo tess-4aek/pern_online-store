3bc12745247a2fdc003fbd9937a3348b899b3d0d
tess-4aek <56252678+tess-4aek@users.noreply.github.com>
2022-07-30 11:53:57 +0300

    add routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db.js');
+const models = require('./models/models.js');
+const cors = require('cors');
+const router = require('./routes/index.js');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 const start = async() => {
     try {
---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+const Router = require('express');
+const router = new Router();
+const deviceRouter = require('./deviceRouter.js')
+const brandRouter = require('./brandRouter.js')
+const typeRouter = require('./typeRouter.js')
+const userRouter = require('./userRouter.js')
+
+router.use('/user', userRouter);
+router.use('/type', typeRouter);
+router.use('/brand', brandRouter);
+router.use('/device', deviceRouter);
+
+module.exports = router;
