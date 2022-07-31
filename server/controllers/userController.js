59c102d879208b14aacb34cdde05a09aa1348d83
tess-4aek <56252678+tess-4aek@users.noreply.github.com>
2022-07-31 09:53:43 +0300

    add jwt registration and login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,20 +1,46 @@
 const ApiError = require('../erorrs/ApiError.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User, Basket } = require('../models/models.js');
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign({ id, email, role }, process.env.SECRET_KEY, { expiresIn: '24h' });
+}
 
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
+        const { email, password, role } = req.body;
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некоректні данні'));
+        }
 
+        const candidate = await User.findOne({ where: { email } });
+        if (candidate) {
+            return next(ApiError.badRequest('Користувач з таким милом вже існує'));
+        }
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({ email, role, password: hashPassword });
+        const basket = await Basket.create({ userId: user.id });
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json({ token });
     }
 
-    async login(req, res) {
-
+    async login(req, res, next) {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            return next(ApiError.internal('Користувача з таким милом не існує'));
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password);
+        if (!comparePassword) {
+            return next(ApiError.internal('Паролі не співпадають'));
+        }
+        const token = generateJwt(user.id, user.email, user.role);
+        return res.json({ token });
     }
 
     async check(req, res, next) {
-        const { id } = req.query;
-        if (!id) {
-            return next(ApiError.badRequest('Не заданий АЙДІ'));
-        }
-        res.json(id);
+
     }
 }
 
