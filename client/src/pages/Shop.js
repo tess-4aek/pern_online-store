391d59ff4ad809c8b823249d4a14c9e9b9b15928
tess-4aek <56252678+tess-4aek@users.noreply.github.com>
2022-08-04 19:29:14 +0300

    finished v1

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -4,6 +4,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { Context } from '..';
 import BrandBar from '../components/BrandBar';
 import DeviceList from '../components/DeviceList';
+import Pages from '../components/Pages';
 import TypeBar from '../components/TypeBar';
 import { fetchBrands, fetchDevices, fetchTypes } from '../http/deviceAPI';
 
@@ -13,8 +14,18 @@ const Shop = observer(() => {
     useEffect(() => {
         fetchTypes().then(data => device.setTypes(data));
         fetchBrands().then(data => device.setBrands(data));
-        fetchDevices().then(data => device.setDevices(data.rows));
+        fetchDevices(null, null, 1, 2).then(data => {
+            device.setDevices(data.rows);
+            device.setTotalCount(data.count);
+        });
     }, [])
+
+    useEffect(() => {
+        fetchDevices(device.selectedType.id, device.selectedBrand.id, device.page, 2).then(data => {
+            device.setDevices(data.rows);
+            device.setTotalCount(data.count);
+        });
+    }, [device.page, device.selectedType, device.selectedBrand]);
 
     return (
         <Container fluid>
@@ -25,6 +36,7 @@ const Shop = observer(() => {
                 <Col md={9}>
                     <BrandBar />
                     <DeviceList />
+                    <Pages />
                 </Col>
             </Row >
         </Container>
